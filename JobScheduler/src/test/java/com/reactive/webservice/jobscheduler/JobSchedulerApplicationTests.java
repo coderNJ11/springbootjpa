2d4709850fe97f234320153eb2cb130802263798
RIshi K <rushikesh.khire@gmail.com>
2025-04-22 09:30:01 -0500

    Job Scheduler JPA h2

---
+++ JobScheduler/src/test/java/com/reactive/webservice/jobscheduler/JobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.webservice.jobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
